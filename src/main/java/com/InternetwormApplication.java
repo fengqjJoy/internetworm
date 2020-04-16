66453d8c8bea34cf774f523c99579c6ea3764dd6
fengqj <409902978@qq.com>
2020-04-16 15:11:17 +0800

    '初始化'

---
+++ src/main/java/com/InternetwormApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetwormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetwormApplication.class, args);
+	}
+
+}
